993242bf24f289d66c5941911f7a13987c854899
jonathanduran7 <joniduran624@gmail.com>
2024-12-26 23:02:58 -0300

    feat(account): add account diff callback

---
+++ app/src/main/java/com/example/mywallet/ui/accounts/AccountDiffCallback.kt
@@ -0,0 +1,20 @@
+package com.example.mywallet.ui.accounts
+
+import androidx.recyclerview.widget.DiffUtil
+
+class AccountDiffCallback(
+    private val oldList: List<Account>,
+    private val newList: List<Account>
+): DiffUtil.Callback(){
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition].name == newList[newItemPosition].name
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] == newList[newItemPosition]
+    }
+}
