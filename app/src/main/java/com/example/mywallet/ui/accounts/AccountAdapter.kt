e8c40e6088313a409f4ca33122b5dd3b3fed1076
jonathanduran7 <joniduran624@gmail.com>
2024-12-26 23:03:49 -0300

    feat(account): add list adapter and comment recycler view adatper

--- app/src/main/java/com/example/mywallet/ui/accounts/AccountAdapter.kt
+++ app/src/main/java/com/example/mywallet/ui/accounts/AccountAdapter.kt
@@ -2,25 +2,41 @@ package com.example.mywallet.ui.accounts
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mywallet.R
 
-class AccountAdapter(private var accounts: List<Account>) : RecyclerView.Adapter<AccountViewHolder>() {
+//class AccountAdapter(private var accounts: List<Account>) : RecyclerView.Adapter<AccountViewHolder>() {
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AccountViewHolder {
+//        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_account, parent, false)
+//        return AccountViewHolder(view)
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return accounts.size
+//    }
+//
+//    override fun onBindViewHolder(holder: AccountViewHolder, position: Int) {
+//        holder.bind(accounts[position])
+//    }
+//
+//    fun updateData(newAccounts: List<Account>) {
+//        val diffCallback = AccountDiffCallback(this.accounts, newAccounts)
+//        val diffResult = DiffUtil.calculateDiff(diffCallback)
+//
+//        this.accounts = newAccounts
+//        diffResult.dispatchUpdatesTo(this)
+//    }
+//}
+
+class AccountAdapter : ListAdapter<Account, AccountViewHolder>(AccountDiffCallbackTwo()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AccountViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_account, parent, false)
         return AccountViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        return accounts.size
-    }
-
     override fun onBindViewHolder(holder: AccountViewHolder, position: Int) {
-        holder.bind(accounts[position])
-    }
-
-    fun updateData(newAccounts: List<Account>) {
-        this.accounts = newAccounts
-        notifyDataSetChanged()
+        holder.bind(getItem(position))
     }
 }
