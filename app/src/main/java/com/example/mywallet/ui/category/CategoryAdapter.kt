326f46c72dbba76e0f668da5724255cf6579e3a8
jonathanduran7 <joniduran624@gmail.com>
2024-12-26 23:28:21 -0300

    refactor(category): change recycleView to listAdatper

--- app/src/main/java/com/example/mywallet/ui/category/CategoryAdapter.kt
+++ app/src/main/java/com/example/mywallet/ui/category/CategoryAdapter.kt
@@ -2,21 +2,16 @@ package com.example.mywallet.ui.category
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.ListAdapter
 import com.example.mywallet.R
 
-class CategoryAdapter(private val category: List<Category>) : RecyclerView.Adapter<CategoryViewHolder>() {
+class CategoryAdapter : ListAdapter<Category, CategoryViewHolder>(CategoryDiffCallBack()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_category, parent, false)
         return CategoryViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        return category.size
-    }
-
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
-        holder.bind(category[position])
+        holder.bind(getItem(position))
     }
-
 }
